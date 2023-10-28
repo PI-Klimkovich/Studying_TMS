b177ebcf52edfcbebc4fa7d0f4bb356fb39085d8
PI-Klimkovich <pol-ik@mail.ru>
2023-10-28 19:40:32 +0300

    Решение ДЗ 9 часть со звездочкой: - Создание файлов стран и запись в них городов. - Копирование шаблона под geojson.

--- tasks_lesson_9_++.py
+++ tasks_lesson_9_++.py
@@ -13,6 +13,54 @@ countries = []
 for text in cites_original:
     if text['country'] not in countries:
         countries.append(text['country'])
+countries.sort()
 
 print(countries)
 print()
+
+cities_in_country = dict()
+for country in countries:
+    cities_in_country[country] = [text['name'] for text in cites_original if text['country'] == country]
+
+# print(cities_in_country)
+
+# Сериализация в JSON
+# json_country: str = json.dumps(country, ensure_ascii=False)
+# print(type(json_country))
+# print(json_country)
+# with open("test.json", "w", encoding="utf-8") as json_file:
+#    json_file.write(json_country)
+
+for country in countries:
+    file_name = 'geo_' + country
+    with open(file_name, "w", encoding="utf-8") as file:
+        for i in range(len(cities_in_country[country])):
+            file.write(cities_in_country[country][i] + '\n')
+    print(file_name)
+
+
+print()
+
+features = []
+
+
+geojson = {
+    "type": "FeatureCollection",  # Обязательный параметр
+    "features": [  # Список данных для отображения на карте
+        {
+            "type": "Feature",  # Обязательный параметр
+            "id": "cityID",  # Идентификатор берем из данных города
+            "geometry": {
+                "type": "Point",  # Обязательно Point, город будет меткой
+                "coordinates": [-65.23, 123.11],
+            },
+            "properties": {
+                "iconCaption": "name",      # Название города
+                "marker-color": "#b51eff",  # Цвет метки
+            },
+        },
+        ...  # Другие элементы коллекции
+    ]
+}
+
+print(geojson)

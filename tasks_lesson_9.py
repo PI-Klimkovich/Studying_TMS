43560b5dcbb381eba260108455ec55503077ea43
PI-Klimkovich <pol-ik@mail.ru>
2023-10-28 17:14:12 +0300

    Решены первые 3 задания из ДЗ 9. Без 'пустых' городов и стран.

--- tasks_lesson_9.py
+++ tasks_lesson_9.py
@@ -13,6 +13,15 @@ count_cities = len(cites_original)
 print('количество городов:', count_cities)
 print()
 
+# без 'пустых' объектов
+count_cities = 0
+for text in cites_original:
+    if text['name'] != '' and text['country'] != '':
+        count_cities += 1
+
+print("количество городов:", count_cities, "без 'пустых' городов и стран")
+print()
+
 # Создать словарь, где ключ — это код страны, а значение — количество городов.
 countries = []
 for text in cites_original:
@@ -32,6 +41,27 @@ print("Словарь количества городов в стране")
 print(count_cities_in_country)
 print()
 
+# без 'пустых' объектов
+countries = []
+for text in cites_original:
+    if text['country'] not in countries and text['country'] != '':
+        countries.append(text['country'])
+
+cities_in_country = []
+for country in countries:
+    total = 0
+    for text in cites_original:
+        if text['country'] == country and text['name'] != '':
+            total += 1
+    cities_in_country.append(total)
+
+count_cities_in_country = dict(zip(countries, cities_in_country))
+print("Словарь количества городов в стране без 'пустых' городов и стран")
+print(count_cities_in_country)
+# Проверка
+# print(sum(cities_in_country))
+print()
+
 # Подсчитать количество городов в северном полушарии и в южном.
 
 # Анализ файла показал, что под "name" записаны не только города!!
@@ -49,5 +79,18 @@ for text in cites_original:
     else:
         count_n += 1
 print(f"Количество городов в северном полушарии {count_n} и южном {count_s}")
-print("но есть сомнения!!")
+print("но есть сомнения по принадлежности к полушарию!!")
+print()
+
+# без 'пустых' объектов
+count_n, count_s = 0, 0
+for text in cites_original:
+    if text['name'] != '' and text['country'] != '':
+        if text['coord']['lat'] < 0:
+            count_s += 1
+        else:
+            count_n += 1
+print(f"Количество городов в северном полушарии {count_n} и южном {count_s} без 'пустых' городов и стран")
+print("но есть сомнения по принадлежности к полушарию!!")
+# print(count_s + count_n)
 print()

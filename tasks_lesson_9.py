78f6ec6534af71a08c26e51bcaab8fd4e77e7e3b
PI-Klimkovich <pol-ik@mail.ru>
2023-10-28 15:38:24 +0300

    Решены первые 3 задания из ДЗ 9. Есть сомнения по координатам полушарий!

--- tasks_lesson_9.py
+++ tasks_lesson_9.py
@@ -4,6 +4,50 @@ with open("city.list.json", "r", encoding="utf-8") as json_file:
     cites = json_file.read()
 
 # Десериализация из JSON
-address_original = json.loads(cites)
+cites_original = json.loads(cites)
 
-print(address_original[:2])
+# print(cites_original[0])
+
+# Определить количество городов в файле.
+count_cities = len(cites_original)
+print('количество городов:', count_cities)
+print()
+
+# Создать словарь, где ключ — это код страны, а значение — количество городов.
+countries = []
+for text in cites_original:
+    if text['country'] not in countries:
+        countries.append(text['country'])
+
+cities_in_country = []
+for country in countries:
+    total = 0
+    for text in cites_original:
+        if text['country'] == country:
+            total += 1
+    cities_in_country.append(total)
+
+count_cities_in_country = dict(zip(countries, cities_in_country))
+print("Словарь количества городов в стране")
+print(count_cities_in_country)
+print()
+
+# Подсчитать количество городов в северном полушарии и в южном.
+
+# Анализ файла показал, что под "name" записаны не только города!!
+# Там скрываются и материки, а также встречаются и "пустышки"!
+
+# Логически:
+# - северное (N) - положительная широта;
+# - южное (S) - отрицательная широта.
+# Широта в базе 'lat'. Но, почему-то, на карте это не работает
+
+count_n, count_s = 0, 0
+for text in cites_original:
+    if text['coord']['lat'] < 0:
+        count_s += 1
+    else:
+        count_n += 1
+print(f"Количество городов в северном полушарии {count_n} и южном {count_s}")
+print("но есть сомнения!!")
+print()

a6029bcea4904e8f25b83befdeeae760e225a25f
PI-Klimkovich <pol-ik@mail.ru>
2023-10-31 16:32:58 +0300

    Решение ДЗ 10 часть 1 и 2: Создание класса Auto

--- tasks_lesson_9_++.py
+++ tasks_lesson_9_++.py
@@ -34,6 +34,12 @@ for text in cites_original:
 
 # print(cities_in_country)
 
+# Разбиение по файлам с учетом "длины" страны.
+
+for key in cities_in_country:
+    a = len(cities_in_country[key])
+    print(a)
+
 # Сериализация в JSON
 # json_country: str = json.dumps(country, ensure_ascii=False)
 # print(type(json_country))
@@ -48,13 +54,9 @@ for text in cites_original:
 #             file.write(cities_in_country[country][i] + '\n')
 #   print(file_name)
 
-
-
-
 print()
 
 features = []
-
 
 geojson = {
     "type": "FeatureCollection",  # Обязательный параметр

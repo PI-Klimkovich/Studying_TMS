c3d2232253ae8c47b3bf5638e9cbf46f7d53b20a
PI-Klimkovich <pol-ik@mail.ru>
2023-11-05 17:39:42 +0300

    ДЗ 11: задания 4 и 5

--- tasks_lesson_11_2.py
+++ tasks_lesson_11_2.py
@@ -3,11 +3,21 @@ class RealString:
     def __init__(self, text: str):
         self.text = text
 
+#    def __eq__(self, other: str):
+#        if isinstance(other, RealString):
+#            return len(self.text) == len(other.text)
+#        else:
+#            return len(self.text) == len(str(other))
+
     def __eq__(self, other: str):
         if isinstance(other, RealString):
-            return len(self.text) == len(other.text)
+            res = len(self.text) == len(other.text)
         else:
-            return len(self.text) == len(str(other))
+            res = len(self.text) == len(str(other))
+        if res is True:
+            return "Длинна строк одинакова"
+        else:
+            return "Длинна строк различна"
 
 
 str_1 = RealString('fish')
@@ -23,5 +33,39 @@ print(str_4 == '123')
 print('Orange' == str_3)
 print('Orange 123' == str_3)
 print(str_3 == 123)
+print('----------------')
+print()
+
+
+# Класс Rectangle (для обработки прямоугольников)
+class Rectangle:
+    def __init__(self, width: int | float, height: int | float):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f'Прямоугольник с шириной {self.width} и высотой {self.height}'
+
+    def get_area(self):
+        return self.height * self.width
+
+    def get_perimeter(self):
+        return 2 * (self.height + self.width)
+
+    @property
+    def is_square(self):
+        return self.height == self.width
+
+
+figura_1 = Rectangle(2, 8)
+figura_2 = Rectangle(12, 8)
+figura_3 = Rectangle(2, 18)
+figura_4 = Rectangle(2, 2)
+
+print(figura_4)
+print(figura_3.get_area())
+print(figura_2.get_perimeter())
+print(figura_1.is_square)
+print(figura_4.is_square)
 print('----------------')
 print()

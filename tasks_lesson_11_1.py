b54c603e535bf0e251e6bd45deac6afbde090cc5
PI-Klimkovich <pol-ik@mail.ru>
2023-11-05 16:04:35 +0300

    ДЗ 11: задания 1-3

--- tasks_lesson_11_1.py
+++ tasks_lesson_11_1.py
@@ -18,10 +18,11 @@ drink_1 = Soda('сироп')
 drink_1.show_my_drink()
 drink_2 = Soda()
 drink_2.show_my_drink()
-
+print('----------------')
 print()
 
 
+# Класс TriangleChecker (возможность построения треугольника из отрезков представленной длины)
 class TriangleChecker:
     def __init__(
             self,
@@ -68,3 +69,142 @@ print(triangle_7.is_triangle())
 print(triangle_8.is_triangle())
 print(triangle_9.is_triangle())
 print(triangle_10.is_triangle())
+print('----------------')
+print()
+
+
+# Класс KgToPounds (перевод килограмм -> фунт)
+class KgToPounds:
+    def __init__(self, kg: int | float):
+        self.__kg = kg
+
+    def to_pounds(self):
+        # 1 kg = 2.2046226 фунта lb
+        return self.__kg * 2.2046226
+
+    def set_kg(self, kg: int | float):
+        self.__kg = kg
+
+    def get_kg(self):
+        return self.__kg
+
+
+weight_1 = KgToPounds(1)
+weight_2 = KgToPounds(5)
+weight_3 = KgToPounds(2.2679618724765)
+
+print(weight_1.to_pounds())
+print(weight_2.to_pounds())
+print(weight_3.to_pounds())
+
+weight_new = KgToPounds(20)
+print(weight_new.get_kg())
+print(weight_new.to_pounds())
+weight_new.set_kg(25)
+print(weight_new.get_kg())
+print(weight_new.to_pounds())
+# print(dir(weight_new))
+print(weight_new._KgToPounds__kg)
+print('----------------')
+print()
+
+
+# !! Класс KgToPoundsNew (перевод килограмм -> фунт) с проверкой ИД
+class KgToPoundsNew:
+    def __init__(self, kg: int | float):
+        if type(kg) in (int, float):
+            self.__kg = kg
+        else:
+            print("Вес должен быть числом! Введена -", type(kg))
+            self.__kg = 'error'
+
+    def to_pounds(self):
+        # 1 kg = 2.2046226 фунта lb
+        if type(self.__kg) in (int, float):
+            return self.__kg * 2.2046226
+        else:
+            return "Вес должен быть числом!"
+
+    def get_kg(self):
+        return self.__kg
+
+    def set_kg(self, kg: int | float):
+        if type(kg) in (int, float):
+            self.__kg = kg
+        else:
+            print("Вес должен быть числом! Введена -", type(kg))
+            self.__kg = 'error'
+
+
+weight_1 = KgToPoundsNew(-1)
+weight_2 = KgToPoundsNew(5)
+weight_3 = KgToPoundsNew(2.2679618724765)
+weight_4 = KgToPoundsNew('5')
+
+print('1', weight_1.to_pounds())
+print('2', weight_2.to_pounds())
+print('3', weight_3.to_pounds())
+print('4', weight_4.to_pounds())
+
+weight_new = KgToPoundsNew(20)
+print('6', weight_new.get_kg())
+print('7', weight_new.to_pounds())
+weight_new.set_kg('25')
+print('8', weight_new.get_kg())
+print('9', weight_new.to_pounds())
+print('----------------')
+print()
+
+
+# !!! Класс KgToPoundsNewDec (перевод килограмм -> фунт) с проверкой ИД и декораторами
+class KgToPoundsNewDec:
+    def __init__(self, kg: int | float):
+        if type(kg) in (int, float):
+            self.__kg = kg
+        else:
+            print("Вес должен быть числом! Введена -", type(kg))
+            self.__kg = 'error'
+
+    def to_pounds(self):
+        # 1 kg = 2.2046226 фунта lb
+        if type(self.__kg) in (int, float):
+            return self.__kg * 2.2046226
+        else:
+            return "Вес должен быть числом!"
+
+    @property
+    # ----- Геттер -----
+    def kg(self):
+        return self.__kg
+
+    @kg.setter
+    # ----- Сеттер -----
+    def kg(self, kg: int | float):
+        if type(kg) in (int, float):
+            self.__kg = kg
+        else:
+            print("Вес должен быть числом! Введена -", type(kg))
+            self.__kg = 'error'
+
+
+weight_1 = KgToPoundsNewDec(-1)
+weight_2 = KgToPoundsNewDec(5)
+weight_3 = KgToPoundsNewDec(2.2679618724765)
+weight_4 = KgToPoundsNewDec('5')
+
+print('1', weight_1.to_pounds())
+print('2', weight_2.to_pounds())
+print('3', weight_3.to_pounds())
+print('4', weight_4.to_pounds())
+
+weight_new = KgToPoundsNewDec(20)
+print('6', weight_new.kg)
+print('7', weight_new.to_pounds())
+weight_new.kg = '25'
+print('8', weight_new.kg)
+print('9', weight_new.to_pounds())
+weight_new.kg = 25
+print('10', weight_new.kg)
+print('11', weight_new.to_pounds())
+print('----------------')
+print()

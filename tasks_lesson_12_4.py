c5e8fc7218a005bb1c63c39f6d21f8a65c133dfc
PI-Klimkovich <pol-ik@mail.ru>
2023-11-13 16:14:43 +0300

    ДЗ 12 Задание 5

---
+++ tasks_lesson_12_4.py
@@ -0,0 +1,53 @@
+from tasks_lesson_12_1 import Product, Pizza, Coffee
+from tasks_lesson_12_2 import AbstractShop
+
+
+class NonProductError(TypeError):
+    pass
+
+
+class RealShop(AbstractShop):
+    def __init__(self):
+        self._products = []
+
+    @staticmethod
+    def is_valid(product: Product) -> None:
+        """ Проверка типа передаваемых дынных """
+        if not (isinstance(product, Product)):
+            raise NonProductError(f'"{product}" - не является продуктом')
+
+    def add_product(self, data: Product):
+        """ Добавление нового товара"""
+        self.is_valid(data)
+        return self._products.append(data)
+
+    def sell_product(self, data):
+        """ Продажа товара """
+        for product in self._products:
+            self.is_valid(data)
+            if product.id == data:
+                self._products.remove(product)
+                return self._products
+
+    def all_products(self):
+        """ Перечень всех товаров """
+        print(self._products)
+
+
+products = RealShop()
+
+p_001 = Pizza(6605, 'пицца', 12.66, ['сыр', 'ананас', 'грибы', 'креветки'], 'острая', 12)
+p_003 = Coffee(8844, 'кофе', 2.66, 0.33, 'с сахаром', 'классическое')
+p_01 = "666"
+# p_02 = 666
+
+RealShop.all_products(products)
+RealShop.add_product(products, p_001)
+RealShop.all_products(products)
+RealShop.add_product(products, p_003)
+RealShop.all_products(products)
+RealShop.add_product(products, p_01)
+RealShop.all_products(products)
+# RealShop.add_product(products, p_02)
+# RealShop.all_products(products)
+print()
